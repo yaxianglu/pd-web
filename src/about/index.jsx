7aa57cad635aa2184126aa551eb6c53df120963b
Max Lu <maxlu@shein.com>
2025-08-17 14:20:24 +0800

    f

--- src/about/index.jsx
+++ src/about/index.jsx
@@ -14,6 +14,8 @@ import VideoList from '../components/video-list';
 import Description from './description';
 import p3 from './imgs/3.jpg';
 import p4 from './imgs/4.svg';
+import Creater from './creater';
+import CardWrapper from '../components/card-wrapper';
 
 export default function PageCom() {
 
@@ -39,6 +41,7 @@ export default function PageCom() {
       <Header2 />
       <PageWrapper>
         <Top />
+        <Creater />
         <Innovation />
         {
           isMobile ? (
@@ -50,26 +53,31 @@ export default function PageCom() {
         <ManufacturingSection />
         <Sketch
           title={<>為什麼選擇珍舒美隱形牙套？</>}
-          subtitle={<>珍舒美結合最新人工智慧設計演算法與超過30年矯治規劃經驗的專業團隊，<br />
-            為每位患者量身打造個人定制治療方案。治療設計可整合牙根CBCT掃描與側<br />
-            位頭影X光片等三維影像資料，提升矯正的科學性與全面性。</>}
+          subtitle={(
+            <CardWrapper title="醫療級軟體" style={{ marginTop: 0 }}>
+              <div className="about-manufacturing-section-content">
+                珍舒美結合最新人工智慧設計演算法與超過30年矯治規劃經驗的專業團隊，為每位患者量身打造個人定制治療方案。治療設計以AI智慧系統為輔助可整合牙根CBCT掃描與側位頭影X光片等三維影像資料，提升矯正的科學性與全面性。<br/><br/>
+                珍舒美透過與台灣醫療器材工廠協作，建立龐大的亞洲齒顎資料庫，針對亞洲人常見的齒列問題與臉部輪廓進行優化，包括上顎突出、下巴後縮等狀況，矯正後的成果能更完美，打造符合亞洲人臉部輪廓的微笑曲線。
+              </div>
+            </CardWrapper>
+          )}
         />
         <VideoList />
         <Sketch
+          direction='right'
           title={<>品質穩定、交期可靠</>}
-          subtitle={<>我們的每一副牙套，皆於通過ISO 13485與GMP認證的台灣醫療器材工廠製作，<br />
+          subtitle={<>我們的每一副牙套，皆於通過ISO 13485與GMP認證的台灣牙科工廠製作，<br />
             從原料到製程皆嚴格控管，確保品質穩定、交期可靠。</>}
         />
-        <div style={{ width: '100%', height: 'auto', marginTop: '40px', display: 'flex', justifyContent: 'start' }}>
-            <img src={p3} alt="#" style={{ width: '70%' }}/>
+        <div style={{ width: '100%', height: 'auto', marginTop: '40px', display: 'flex', justifyContent: 'end' }}>
+        <img src={p3} alt="#" style={{ width: '70%' }}/>
         </div>
         <Sketch
-          direction='right'
           title={<>醫療級軟體</>}
           subtitle={<>產品採用無BPA高透明度醫療級材料，具有極高透明度，兼具隱形美觀與舒適貼合<br />
             特性，不易染色，易於清潔，讓患者在治療過程中也能自在微笑。</>}
         />
-        <div style={{ width: '100%', height: 'auto', marginTop: '40px', display: 'flex', justifyContent: 'end' }}>
+        <div style={{ width: '100%', height: 'auto', marginTop: '40px', display: 'flex', justifyContent: 'start' }}>
             <img src={p4} alt="#" style={{ width: '50%' }}/>
         </div>
       </PageWrapper>
--- src/about/top-new.jsx
+++ src/about/top-new.jsx
@@ -12,6 +12,9 @@ export default function Top() {
           <div className="about-top-cotent-left-ddd">
             珍舒美是由美國矽谷科技公司 PEARL DIGITAL INC. 所創立。
           </div>
+          <div className="about-top-cotent-left-text">
+            珍舒美創立於美國矽谷，聯合創辦人兼指導醫師Dr. David Shen專注於牙齒矯正領域30年，有豐富的臨床經驗，是 OrthoWorks 矯正專科集團的創辦人兼執行長，該集團擁有11家診所，是舊金山灣區歷史最悠久且仍由私人經營的矯正專科診所。
+          </div>
             <img src={p1} alt="" />
           </div>
         </div>
