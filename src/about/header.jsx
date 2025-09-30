d13f7a08f94981ddbfea88ec6147c726d607fd74
yaxianglu <yaxianglu@users.noreply.github.com>
2025-09-30 23:17:17 +0800

    f

--- src/about/header.jsx
+++ src/about/header.jsx
@@ -6,16 +6,41 @@ import { useLanguage } from '../context/LanguageContext';
 export default function Description() {
   const { t } = useLanguage();
   
+  // return (
+  //   <div className="about-about-description-section">
+  //     <div className="about-about-description-section-text">
+  //     <div className="about-about-description-section-text-title">
+        // {t('about.pageTitle')}<br />
+  //       </div>
+  //       <div className="about-about-description-section-text-content">
+  //       {t('about.pageSubtitle')}
+  //       </div>
+  //     <DetailButton text={t('about.becomePartner')} onClick={() => {
+  //       window.location.href = '/join';
+  //     }}/>
+  //     </div>
+  //     <img src={p5} alt="#" />
+  //   </div>
+  // );
+
+
   return (
-    <div className="about-about-description-section">
-      <div className="about-about-description-section-text">
+    <div className="join-join-description-section">
+      <div className="join-join-description-section-text" style={{ backgroundImage: `url(${p5})`, backgroundSize: '100%', backgroundRepeat: 'no-repeat' }}>
+      <div className="join-join-description-section-text-title">
         {t('about.pageTitle')}<br />
+        </div>
+        <div className="join-join-description-section-text-content">
         {t('about.pageSubtitle')}
-      <DetailButton text={t('about.becomePartner')} onClick={() => {
-        window.location.href = '/join';
-      }}/>
+        </div>
+        <DetailButton 
+          text={t('join.hero.buttonText')} 
+          size="small" 
+          onClick={() => {
+            window.location.href = '/join';
+          }}
+        />
       </div>
-      <img src={p5} alt="#" />
     </div>
   );
 }
