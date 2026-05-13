ab2ed12021e2d30f7c95009caf43fa61ca34833b
max lu <yaxiang.lu@sigmatrx.ai>
2026-05-13 17:19:58 +0800

    feat: build

--- src/contants/index.js
+++ src/contants/index.js
@@ -1,8 +1,9 @@
-
 // API基础URL
-export const API_BASE_URL = process.env.REACT_APP_API_URL || '';
-// export const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://98.83.160.201:3001';
-// export const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+export const API_BASE_URL = isDevelopment
+  ? ''
+  : (process.env.REACT_APP_API_URL || 'http://98.83.160.201:3001');
 
 
 export const wrapperStyle = {
